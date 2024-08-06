cd92a55d320702183c4a737053ac5ee1c1624461
Yuvraj-Sandhu <yuvrajsandhu2502@gmail.com>
2024-08-06 15:11:39 -0400

    Socio added

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,20 @@
+<main>
+  <div class="title">
+    <h1>
+      Socio
+    </h1>
+  </div>
+</main>
 
+
+
+
+
+
+<style>
+  .title{
+    height: 72px;
+    background-color: rgba(255,255,255,1);
+    
+  }
+</style>
