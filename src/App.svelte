d26c16bcd9a1719ad15afa9e5e35f2e516c09011
Yuvraj-Sandhu <yuvrajsandhu2502@gmail.com>
2024-08-06 15:23:31 -0400

    columns added

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,17 @@
       Socio
     </h1>
   </div>
+
+  <div class="row">
+    <div class="column-left">
+      <p>something</p>
+    </div>
+
+    <div class="column-right">
+      <p>something else</p>
+    </div>
+
+  </div>
 </main>
 
 
@@ -16,5 +27,25 @@
     height: 72px;
     background-color: rgba(255,255,255,1);
     
+  }
+
+  .column-left{
+    float: left;
+    width: 35%;
+    height: 600px;
+    background-color: aqua;
+  }
+
+  .column-right{
+    float: right;
+    width: 65%;
+    height: 600px;
+    background-color: beige;
+  }
+
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
   }
 </style>
